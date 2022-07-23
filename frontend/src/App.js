bd0602d4c5424d48e8a212d4dea6e50a06e7877d
hqu118 <Harryqu666@gmail.com>
2022-07-23 18:19:31 +1200

    added routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Teacher from "./pages/Teacher";
+import Main from "./pages/Main";
+import Error from "./pages/Error";
+import About from "./pages/About";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="about" element={<About />} />
+        <Route path="teacher" element={<Teacher />} />
+        <Route path="main" element={<Main />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
     </div>
   );
 }
