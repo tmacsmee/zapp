7c802be44507e7b2723ab0affed0e49901caabc1
Harry Qu <Harryqu666@gmail.com>
2022-07-24 12:37:52 +1200

    added in memory user authorisation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
@@ -8,20 +9,23 @@ import Error from "./pages/Error";
 import About from "./pages/About";
 import TermsAndConditions from "./pages/TermsAndConditions.jsx";
 import Nav from "./components/Nav/Nav";
+import { UserProvider } from "./contexts/UserContext";
 
 function App() {
   return (
     <div className="App">
-      <Nav />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="login" element={<Login />} />
-        <Route path="about" element={<About />} />
-        <Route path="teacher" element={<Teacher />} />
-        <Route path="main" element={<Main />} />
-        <Route path="terms" element={<TermsAndConditions />} />
-        <Route path="*" element={<Error />} />
-      </Routes>
+      <UserProvider>
+        <Nav />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="login" element={<Login />} />
+          <Route path="about" element={<About />} />
+          <Route path="teacher" element={<Teacher />} />
+          <Route path="main" element={<Main />} />
+          <Route path="terms" element={<TermsAndConditions />} />
+          <Route path="*" element={<Error />} />
+        </Routes>
+      </UserProvider>
     </div>
   );
 }
--- frontend/src/pages/Teacher.jsx
+++ frontend/src/pages/Teacher.jsx
@@ -1,5 +1,8 @@
 import React from "react";
+import { UserContext } from "../contexts/UserContext";
+import { Navigate } from "react-router-dom";
 
 export default function Teacher() {
-  return <div>Teacher</div>;
+  const [state, dispatch] = React.useContext(UserContext);
+  return <>{!state.active && <Navigate to="/login" replace={true} />}</>;
 }
